3b587b7b43bac834548f3e600a6054fd2050d0f8
PJ568 <PJ568@PJ568.eu.org>
2024-09-13 11:17:14 +0800

    【新增】覆盖 curl github 加速

---
+++ githubfast_download.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+# 该脚本用于处理 makepkg 安装软件时，由 github 下载缓慢甚至无法下载的问题
+# 检测域名是不是 github，如果是，则替换为镜像网站，依旧使用 curl 下载
+# 如果不是 github 则采用 curl 进行下载
+# 实验用链接：
+# https://githubfast.com/PJ-568/PJ568-sh/archive/refs/heads/main.zip
+# https://github.com/PJ-568/PJ568-sh/archive/refs/heads/main.zip
+
+domin=`echo $2 | cut -f3 -d'/'`;
+others=`echo $2 | cut -f4- -d'/'`;
+case "$domin" in 
+    "github.com")
+    url="https://githubfast.com/"$others;
+    echo "download from github mirror $url";
+    /usr/bin/curl -gqb "" -fLC - --retry 3 --retry-delay 3 -o $1 $url;
+    ;;
+    *)
+    url=$2;
+    /usr/bin/axel -n 15 -a -o $1 $url;
+    ;;
+esac
